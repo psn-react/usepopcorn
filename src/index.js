f27d97c88a5e258fdecd3ff6c210a642f1a4d75d
Pyae Sone Nyein <pyaesonenyein1996@gmail.com>
2024-07-10 14:37:30 +0630

    star customize props ~~~~ 1:05 mins

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import StarRating from './components/StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5}/>
+    <StarRating maxRating={3}  messages={['terrible','bad','ok','good','cool']} defaultRating={3}/>
+
+    <StarRating maxRating={10} color="green" size="25" defaultRating={4}/>
     {/* <App /> */}
   </React.StrictMode>
 );
